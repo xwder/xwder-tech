d067999d12b47bb958c4c49913ea7dbe366a0ab6
xwder <xwdercom@gmail.com>
2021-02-10 10:14:58 +0800

    update junit dependency

---
+++ java-new-features/src/main/java/com/xwder/newfeatures8/Test.java
@@ -0,0 +1,17 @@
+package com.xwder.newfeatures8;
+
+/**
+ * @author xwder
+ * @date 2021/2/8 16:57
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        String v1 = "202102071650";
+        String v2 = "202102090940";
+        String v3 = "202102091040";
+        System.out.println(v1.compareTo(v2));
+        System.out.println(v2.compareTo(v2));
+        System.out.println(v2.compareTo(v3));
+    }
+}
